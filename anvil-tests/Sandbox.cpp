d2413ba295fdd43e500b5bafea10edf1ac6bb57c
Joseph Steurer <joseph.a.steurer@gmail.com>
2021-03-04 22:19:19 -0600

    add lua

--- anvil-tests/Sandbox.cpp
+++ anvil-tests/Sandbox.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" {
+#include "../lua/lua/lua.h"
+#include "../lua/lua/lualib.h"
+#include "../lua/lua/lauxlib.h"
+}
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace anvil_tests
@@ -11,7 +17,8 @@ namespace anvil_tests
 
         TEST_METHOD(Tests)
         {
-
+            lua_State* state = luaL_newstate();
+            lua_close(state);
         }
     };
 }
