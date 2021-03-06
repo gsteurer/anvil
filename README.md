ed1f9ffb1c35d981497b8829a446be90cb3cff5f
Joseph Steurer <joseph.a.steurer@gmail.com>
2021-03-05 21:45:37 -0600

    replace MS CppUnitTest with googletest

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-{\rtf1}
+#### Frustrations:
+
+* The Microsoft CppUnitTest is, in practice, worthless because you're limited to one assert per test. In other words, someone's ego prescribes how I should write my tests. Don't use it. 
--- anvil/include/ads/graph.h
+++ anvil/include/ads/graph.h
@@ -1,43 +1,63 @@
 #pragma once
-#include <vector>
+#include <set>
 #include <unordered_map>
 
 typedef int GraphIdentifier;
 typedef int EdgeWeight;
-template<typename T> 
-struct GraphVertex {
-	std::vector<GraphIdentifier> connections;
-};
 
 struct GraphEdge {
 	EdgeWeight weight;
+	GraphIdentifier neighbor;
 };
+
+template<typename T> 
+struct GraphVertex {
+	std::set<GraphEdge> connections;
+	bool AddConnection(GraphIdentifier id) {
+		auto result = this->connections.insert({0, id});
+		return result->second;
+	}
+	bool RemoveConnection(GraphIdentifier id) {
+		return false;
+	}
+};
+
+
 
 template<typename T>
 class Graph {
 public:
 	Graph() {}
 	~Graph() {
-		for (auto it = this->verticies.begin(i); it != this->verticies.end(i); ++it) {
+		for (auto& it = this->verticies.begin(i); it != this->verticies.end(i); ++it) {
 			delete it->second;
 		}
 	}
-	void Add(GraphIdentifier id) {
-		if (this->verticies[id] == NULL) {
+	bool Add(GraphIdentifier id) {
+		if (this->Get(id) == NULL) {
 			this->verticies[id] = new GraphVertex<T>();
+			return true;
 		}
-
+		return false;
 	}
-	void Remove(GraphIdentifier id) {
-		if (this->verticies[id] != NULL) {
+	bool Remove(GraphIdentifier id) {
+		if (this->Get(id) != NULL) {
 			delete this->verticies[id];
 			this->verticies[id] = NULL;
+			return true;
 		}
+		return false;
 	}
 	GraphVertex<T>* Get(GraphIdentifier id) {
 		return this->verticies[id];
 	}
-	void Connect(GraphIdentifier lhs, GraphIdentifier rhs) {}
+	void Connect(GraphIdentifier lhs, GraphIdentifier rhs) {
+		lhsVert = this->Get(lhs);
+		rhsVert = this->Get(rhs);
+		if (lhsVert != NULL && rhsVert != NULL) {
+			lhsVert->
+		}
+	}
 	std::unordered_map<GraphIdentifier, GraphVertex<T>*> verticies;
 
 };
