0606082996118da702d194302d4108efc6f21501
Joseph A. (Gus) Steurer <joseph.a.steurer@outlook.com>
2021-06-14 23:29:06 -0500

    rename delete to remove. add todos

--- anvil/include/ads/map.h
+++ anvil/include/ads/map.h
@@ -17,12 +17,14 @@ long Hash(int key) {
     return 55;
 }
 
+// https://en.wikipedia.org/wiki/Hash_table
 template <typename K, typename V>
 struct Map {
     Map();
     Map(const Map<K, V>& m);
     ~Map();
     float LoadFactor();
+    // @@@ need function to set load factor threshold
     /*
         When an insert is made such that the number of entries in a hash table exceeds the product of the load factor and the current capacity then the hash table will need to be rehashed.[9] Rehashing includes increasing the size of the underlying data structure[9] and mapping existing items to new bucket locations
         To limit the proportion of memory wasted due to empty buckets, some implementations also shrink the size of the table—followed by a rehash—when items are deleted.
@@ -30,10 +32,12 @@ struct Map {
     void Rehash(int size);
     // the average cost of a lookup depends only on the average number of keys per bucket—that is, it is roughly proportional to the load factor.
     // a chained hash table with 1000 slots and 10,000 stored keys (load factor 10) is five to ten times slower than a 10,000-slot table (load factor 1); but still 1000 times faster than a plain sequential list.
+    // @@@ const Option<V> operator[](int index) const;
     const V operator[](int index) const;
+    // @@@ Option<V&> operator[](int index);
     V& operator[](int index);
     void Insert(K key, V value);
-    V Delete(K key);
+    V Remove(K key);
 
    private:
     // let's store collisions in a linked list
@@ -85,5 +89,5 @@ void Map<K, V>::Insert(K key, V value) {
 }
 
 template <typename K, typename V>
-V Map<K, V>::Delete(K key) {
+V Map<K, V>::Remove(K key) {
 }
